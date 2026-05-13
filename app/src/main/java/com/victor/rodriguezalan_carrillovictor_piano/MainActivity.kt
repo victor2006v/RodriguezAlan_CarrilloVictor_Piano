e9c88ed5c34495bfbda94cf3cb44b9556617a0bc
Victor Carrillo <a22viccarnav@inspedralbes.cat>
2026-05-13 12:08:07 +0200

    Upload project

---
+++ app/src/main/java/com/victor/rodriguezalan_carrillovictor_piano/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.victor.rodriguezalan_carrillovictor_piano
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
